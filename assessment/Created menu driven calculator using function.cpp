d75a700d59c78e8e431a7b1c9241acaf5aeda034
kunj0072003 <kunjprajapati007@gmail.com>
2025-04-07 06:36:06 +0530

    Update Created menu driven calculator using function.cpp

--- assessment/Created menu driven calculator using function.cpp
+++ assessment/Created menu driven calculator using function.cpp
@@ -1,87 +1,45 @@
-#include <stdio.h>
+#include<stdio.h>
+main(){
+int choice;
+int num1, num2;
 
-int Addition(int a, int b);
-int Substraction(int a, int b);
-int Multiplication(int a, int b);
-float Division(int a, int b);
-
-main() 
-{
-    int a, b, choice;
-
-    printf("---------------------------Created menu-driven calculator using functions---------------------------\n");
+while(1){
+    printf("\n-----MENU-----\n");
+    printf("1.Addition\n");
+    printf("2.subtraction\n");
+    printf("3.Multipalication\n");
+    printf("4.Divison\n");
     
-    printf("\nEnter number 1: ");
-    scanf("%d", &a);
-    printf("Enter number 2: ");
-    scanf("%d", &b);
-
-    do 
-	{
-        printf("\nSelect Option:\n");
-        printf("1. Addition\n");
-        printf("2. Substraction\n");
-        printf("3. Multiplication\n");
-        printf("4. Division\n");
-        printf("5. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-
-        if (choice == 1) 
-		{
-            printf("Addition: %d\n", Addition(a, b));
-        } 
-		else if (choice == 2) 
-		{
-            printf("Substraction: %d\n", Substraction(a, b));
-        } 
-		else if (choice == 3) 
-		{
-            printf("Multiplication: %d\n", Multiplication(a, b));
-        } 
-		else if (choice == 4) 
-		{
-            if (b != 0) 
-			{
-                printf("Division: %.2f\n", Division(a, b));
-            } 
-			else 
-			{
-                printf("Error: Division by zero is not allowed.\n");
-            }
-        } 
-		else if (choice == 5) 
-		{
-            printf("Exiting the program. Thank you!\n");
-        } 
-		else 
-		{
-            printf("Invalid choice! Please try again.\n");
+    printf("enter your choice: ");
+    scanf("%d",&choice);
+    
+    if(choice<1 || choice>4){
+        printf("invalid choice !!\n");
+    }
+    
+    printf("enter first number:");
+    scanf("%f",&num1);
+    printf("enter second number;");
+    scanf("%f",&num2);
+    
+    if(choice==1){
+        printf("Addition = %d\n",num1+num2);
+    }
+    else if(choice==2){
+        printf("subtraction = %d\n",num1-num2);
+    }
+    else if(choice==3){
+        printf("Multiplication = %d\n",num1*num2);
+    }
+    else{
+        if(num2!=0)
+        {
+            printf("Devison = %d\n",num1/num2);
         }
-
-    } 
-	while (choice != 5);
-
-    return 0;
+        else
+        {
+            printf("error : divison by zero is not allowed\n");
+        }
+    }
+}    
 }
-
-int Addition(int a, int b) 
-{
-    return a + b;
-}
-
-int Substraction(int a, int b) 
-{
-    return a - b;
-}
-
-int Multiplication(int a, int b) 
-{
-    return a * b;
-}
-
-float Division(int a, int b) 
-{
-    return (float)a / b;
-}
-
